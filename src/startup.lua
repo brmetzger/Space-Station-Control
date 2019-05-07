d2c524e24ff3138a1b618265c42071ceada92a6b
gitrog <37446909+gitrous@users.noreply.github.com>
2019-05-07 11:45:14 -0400

    Print the help command

--- src/startup.lua
+++ src/startup.lua
@@ -1,10 +1,11 @@
 local functions = {
     --Get a list of all commands
     help = function()
-        local outputString = "The recognized commands are:"
+        local outputString = "The recognized commands are:";
         for funcName,func in pairs(functions) do
             outputString = outputString .. "\n" .. funcName;
         end;
+        print(outputString);
     end;
 };
 
