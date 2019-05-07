646a68482a11218f314cb91421b1fa632d2db8b9
gitrog <37446909+gitrous@users.noreply.github.com>
2019-05-07 09:53:45 -0400

    Create the startup file

---
+++ src/startup.lua
@@ -0,0 +1,25 @@
+local functions = {
+    --Get a list of all commands
+    help = function()
+        local outputString = "The recognized commands are:"
+        for funcName,func in pairs(functions) do
+            outputString = outputString .. "\n" .. funcName;
+        end;
+    end;
+};
+
+--Initialize the program
+term.clear();
+term.setCursorPos(1,1);
+print("SS Control Panel v1.0");
+while true do
+    --Check for commands
+    local input = read();
+    if functions[input:lower()] then
+        --Recognized command
+        functions[input:lower()]();
+    else
+        --Unrecognized command
+        print("\"" .. input:lower() .. "\" is an unknown command. Please type \"help\" for a list of commands.");
+    end;
+end;
